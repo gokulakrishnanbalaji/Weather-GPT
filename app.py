135833cb3d991cc65192f8919d6a904fae2d019f
Gokulakrishnan Balaji <gokulbalaji2408@gmail.com>
2023-06-01 23:48:07 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,60 @@
+import requests
+from datetime import datetime, timedelta
+
+def get_weather_forecast(city_name, api_key):
+    # Make a request to the OpenWeatherMap API to retrieve the weather forecast
+    base_url = "http://api.openweathermap.org/data/2.5/forecast"
+    params = {
+        "q": city_name,  # Specify the city name for the forecast
+        "appid": api_key,  # Include your OpenWeatherMap API key
+        "units": "metric"  # Use metric units for temperature
+    }
+    response = requests.get(base_url, params=params)  # Send the API request
+    weather_data = response.json()  # Parse the response as JSON
+
+    # Extract the relevant weather information from the API response
+    forecast = []
+    for prediction in weather_data["list"]:
+        forecast.append({
+            "date": datetime.fromtimestamp(prediction["dt"]),
+            "description": prediction["weather"][0]["description"],
+            "temp_min": prediction["main"]["temp_min"],
+            "temp_max": prediction["main"]["temp_max"],
+            "humidity": prediction["main"]["humidity"]
+        })
+    return forecast
+
+def print_weather_forecast(forecast):
+
+#print the forecast header for the five days along with details in a for loop
+    print("-" * 68)
+    print(f" Weather forecast for {forecast[0]['date'].strftime('%A, %B %d')}")
+    print("-" * 68)
+    print("TIME        DESCRIPTION                          TEMP (C)    HUMIDITY")
+    print("-" * 68)
+
+     # Print each of the forecasts for the next five days in a for loop for 12 hour time intervals
+    for daily_forecast in forecast:
+        date_string = daily_forecast["date"].strftime("%H:%M")
+        description = daily_forecast["description"].capitalize()
+        temperature = daily_forecast["temp_max"]
+        humidity = daily_forecast["humidity"]
+        print(f"{date_string:8}    {description:30} {temperature:6}      {humidity:6}%")
+    print("-" * 68)
+
+
+def main():
+    # Enter your OpenWeatherMap API key here
+    api_key = '2fd12545a95e925217919cfe171a10a6'
+
+    # Prompt the user to enter a city name
+    city_name = input("Enter the name of a city whose weather you want to see: ")
+
+    # Get the weather forecast for the next five days
+    forecast = get_weather_forecast(city_name, api_key)
+    
+    # Print the forecast to the screen
+    print_weather_forecast(forecast)
+
+if _name_ == '_main_':
+    main()
